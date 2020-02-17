929e46cc5b73893b71c002036f8812cd0ffe9254
Emilio Rosa <me@emiliort.com>
2020-02-17 17:51:15 -0400

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace poo_csharp_p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
